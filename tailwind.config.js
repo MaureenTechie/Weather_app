6413b47fbaab359c513be99bfd66a18285052818
Maureen Nyanamba <maureenmoraa006@gmail.com>
2025-05-09 10:16:17 +0300

    manually configured ripple ui in the tailwind.config.js file and edited use of turbopack in package.json file under the dev scripts and added imports to enable use of the ripple ui

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,14 @@
+/* eslint-disable @typescript-eslint/no-require-imports */
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: ['./pages/**/*.{js, ts, jsx, tsx}', './components/**/*.{js, ts, jsx, tsx}'],
+    content: [
+        './app/**/*.{js, ts, jsx, tsx}',  // for app dir
+        './pages/**/*.{js, ts, jsx, tsx}', // for pages dir
+        './components/**/*.{js, ts, jsx, tsx}'
+    ],
     theme: {
       extend: {},
     },
-    plugins: [],
+    plugins: [require("rippleui")],
   }
   
