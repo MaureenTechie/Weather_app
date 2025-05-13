3c1442dfc46f0006afa2d1de2455cadce142252e
Maureen Nyanamba <maureenmoraa006@gmail.com>
2025-05-13 15:24:47 +0300

    Inpute data that shall fetch Api endpoint to display weather at different locations

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,6 +2,9 @@
 // import { Component } from 'react';
 import type { AppProps } from 'next/app';
 import './styles/globals.css';
+import '@fortawesome/fontawesome-svg-core/styles.css';
+import {config} from '@fortawesome/fontawesome-svg-core';
+config.autoAddCss = false;
 
 export default function App({ Component, pageProps }: AppProps){
     return <Component {...pageProps} />;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,76 @@
+import { useState } from "react";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faMagnifyingGlassLocation } from '@fortawesome/free-solid-svg-icons';
+
+
 export default function Home(){
-    return <h1 className="text-3xl font-bold text-center mt-10">Hello, World!</h1>
+    const [city, setCity] = useState('');
+    const [weather, setWeather] = useState(null);
+    const [error, setError] = useState('');
+
+    const fetchWeatherData = async (e) => {
+        e.preventDefault();
+        setError('');
+        setWeather(null);
+
+        // Fetching weather data using local API endpoint
+        // try{
+        //     const res = await fetch(`http://127.0.0.1:8001/weather?city=${city}`);
+        //     if (!res.ok) throw new Error('Failed to fetch data.');
+        //     const data = await res.json();
+        //     setWeather(data);
+        // } catch (err){
+        //     setError(err.message);
+        // }
+
+        try{
+            const apiKey = '29f2960907afaf556a470c20ccb90c55';
+            const res = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`);
+
+            if (!res.ok) throw new Error('Failed to fetch data.');
+            const data = await res.json();
+
+            const weatherData = {
+                city: data.name,
+                temperature: data.main.temp,
+                description: data.weather[0].description,
+                humidity: data.main.humidity,
+                wind_speed: data.wind.speed,
+            };
+
+            setWeather(weatherData);
+        }catch (err){
+            setError(err.message);
+        }
+    };
+
+    return (
+        <div style={{padding: '2rem', fontFamily:'Arial'}}>
+            <h1>Weather App</h1>
+            <form onSubmit={fetchWeatherData} style={{display: 'flex',alignItems: 'center'}}>
+                <FontAwesomeIcon icon={faMagnifyingGlassLocation} style={{marginRight: '0.5rem'}}/>
+                <input
+                type="text"
+                placeholder="Search city"
+                value={city}
+                onChange={(e) => setCity(e.target.value)}
+                required
+                style={{padding: '0.5rem', marginRight:'1rem'}}
+                />
+                <button type="submit" style={{padding: '0.5rem'}}>Get Weather</button>
+            </form>
+
+            {error && <p style={{color: 'red'}}>{error}</p>}
+
+            {weather && (
+                <div style={{marginTop: '2rem'}}>
+                    <h2>Weather in {weather.city}</h2>
+                    <p>Temperature: {weather.temperature} °C </p>
+                    <p>Description: {weather.description}</p>
+                    <p>Humidity: {weather.humidity} %</p>
+                    <p>Wind Speed: {weather.wind_speed} m/s</p>
+                </div>
+            )}
+        </div>
+    )
 }
